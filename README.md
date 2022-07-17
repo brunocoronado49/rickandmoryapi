1fd96e5b7514ff5bcc36ad6d478b150fc96e1011
Bruno Coronado <brunocoronado@MacBook-Air-de-Bruno.local>
2022-07-17 15:18:08 -0500

    Proyecto terminado

---
+++ README.md
@@ -0,0 +1,19 @@
+# Crear un proyecto de React con Vite
+
+# Instrucciónes
+
+1. Desde la linea de comandos o terminal:
+  npm create vite (enter)
+
+2. Inrgesar el nombre del proyecto
+
+3. Elegir un Framework (React)
+
+4. Select a variant (React)
+
+5. npm install para instalar todos los paquetes
+
+6. Para ejecutar el proyecto usar el comando:
+  npm run dev
+
+# Nota: Todos los archivos deben ser de extensión "JSX"
--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -4,9 +4,40 @@ import Character from "./Character";
 const CharacterList = () => {
   const [characters, setCharacters] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+
+  const NavPage = ({page}) => {
+    return (
+      <header className="d-flex justify-content-between align-items-center">
+        <button
+          className="btn btn-primary block"
+          onClick={changePageBack}
+        >
+          Back page
+        </button>
+        <h3>Current page: {page}</h3>
+        <button
+          className="btn btn-primary block"
+          onClick={changePageNext}
+        >
+          Next page
+        </button>
+      </header>
+    );
+  }
+
+  const changePageNext = () => {
+    setPage(page + 1);
+  }
+
+  const changePageBack = () => {
+    setPage(page - 1);
+  }
 
   const fetchData = async () => {
-    const res = await fetch("https://rickandmortyapi.com/api/character");
+    const res = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${page}`
+    );
     const data = await res.json();
     setLoading(false);
     setCharacters(data.results);
@@ -14,10 +45,11 @@ const CharacterList = () => {
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [page]);
 
   return (
     <div className="container">
+      <NavPage page={page}/>
       <div className="row">
         {loading ? (
           <h1>Loading...</h1>
@@ -31,6 +63,8 @@ const CharacterList = () => {
           })
         )}
       </div>
+      <NavPage page={page}/>
+      <br />
     </div>
   );
 };
